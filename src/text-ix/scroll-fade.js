585fd6531114a57699bf5ddafc2d7939d481fe3a
bdcsstudio <info@bodacious.studio>
2024-09-26 14:21:17 +0200

    gsap text-fade update

--- src/text-ix/scroll-fade.js
+++ src/text-ix/scroll-fade.js
@@ -1,21 +1,27 @@
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-console.log('GSAP version:', gsap.version);
+gsap.registerPlugin(ScrollTrigger);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const elements = document.querySelectorAll('.text-fade');
-    console.log('Found text-fade elements:', elements.length);
-
-    elements.forEach((element) => {
-        gsap.to(element, {
-            opacity: 0,
-            duration: 1,
-            scrollTrigger: {
-                trigger: element,
-                start: 'top center',
-                end: 'bottom center',
-                scrub: true,
-            }
-        });
-    });
-});
+document.addEventListener(
+    "scroll",
+    function () {
+        if (!window.textFadeLoaded) {
+            window.textFadeLoaded = true;
+            document.querySelectorAll(".text-fade").forEach((t) => {
+                t.classList.contains("is-button")
+                    ? gsap.fromTo(
+                          t,
+                          { maskImage: "linear-gradient(to top, #0000 0%, #000F 0%)", opacity: 1 },
+                          { maskImage: "linear-gradient(to top, #0000 0%, #000F 100%)", opacity: 0, scrollTrigger: { trigger: t, start: "top -5%", end: "top -40%", scrub: true } }
+                      )
+                    : gsap.fromTo(
+                          t,
+                          { maskImage: "linear-gradient(to top, #0000 0%, #000F 0%)", opacity: 1 },
+                          { maskImage: "linear-gradient(to top, #0000 0%, #000F 100%)", opacity: 0, scrollTrigger: { trigger: t, start: "top 0%", end: "top -30%", scrub: true } }
+                      );
+            });
+        }
+    },
+    { once: true }
+);
