1bd571a57523d5881ee67f74d83887a2002f96e5
bdcsstudio <info@bodacious.studio>
2024-09-26 14:54:29 +0200

    GSAP Register fix

--- src/nav/border.js
+++ src/nav/border.js
@@ -1,8 +1,4 @@
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-// Register the ScrollTrigger plugin
-gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", function () {
     const e = document.querySelector(".vegter-border"),
